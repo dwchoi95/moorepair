3e2e992eb0b4c6e02dba1f2f23729dbbddee2dad
cdw <dwchoi95@gmail.com>
2026-03-22 16:02:24 +0900

    Fix: vllm usage

--- src/llms/models.py
+++ src/llms/models.py
@@ -5,47 +5,39 @@ from pydantic import BaseModel
 class Models:
     @classmethod
     def set(cls,
-                 model:str="codellama/CodeLlama-7b-Instruct-hf", 
-                 temperature:float=0.8,
-                 token_limit:int=4096,
-                 timeout:int=10):
+        model:str="codellama/CodeLlama-7b-Instruct-hf", 
+        temperature:float=0.8,
+        timeout:int=30
+    ):
         from dotenv import load_dotenv
         import os
         load_dotenv()
         LOCAL_API_URL = os.getenv("LOCAL_API_URL")
+        API_KEY = os.getenv("OPENAI_API_KEY")
         
-        cls.client = AsyncOpenAI(
-            timeout=timeout, 
-            base_url=LOCAL_API_URL,
-            max_retries=0)
+        cls.client = AsyncOpenAI(api_key=API_KEY, base_url=LOCAL_API_URL)
         cls.model = model
         cls.temperature = temperature
-        cls.token_limit = token_limit
         cls.timeout = timeout
     
     @classmethod
     async def run(cls, system:str, user:str, format:BaseModel):
         try:
-            response = await cls.client.chat.completions.create(
+            response = await cls.client.chat.completions.parse(
                 model=cls.model, 
                 messages=[
                     { "role": "system", "content": system },
                     { "role": "user", "content": user }
                 ],
                 temperature=cls.temperature,
-                max_tokens=cls.token_limit,
                 timeout=cls.timeout,
-                extra_body={
-                    "truncate_prompt_tokens": 16384,
-                    "structured_outputs": {
-                        "json": format.model_json_schema(),
-                    }
-                }
+                response_format=format,
             )
-            content = response.choices[0].message.content
-            res = format.model_validate_json(content)
-            return res
+            content = response.choices[0].message
+            if content.parsed:
+                return content.parsed
+            return None
         except Exception as e:
-            # print(e)
+            # print(e) # DEBUG
             pass
         return None
